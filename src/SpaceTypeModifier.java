618904800b98b6347867281de153bc42346ed5d4
Simbrion <melnikovichAS@gmail.com>
2025-02-28 22:31:32 +0100

    Typos and minor fixes

--- src/SpaceTypeModifier.java
+++ src/SpaceTypeModifier.java
@@ -5,11 +5,12 @@ public class SpaceTypeModifier {
     public void modifyType(Space space) throws IOException {
 
         while (true) {
-            System.out.println("""
+            System.out.println(Config.YELLOW_COLOUR +
+                               """
                                Do you want to change the type of the selected space?
                                  1. Yes
                                  2. No
-                               """);
+                               """ + Config.RESET_COLOUR);
             String userInput = Main.READER.readLine();
             if (userInput.equals("1")) {
                 Main.SPACE_TYPE_SELECTOR.selectType(space);
